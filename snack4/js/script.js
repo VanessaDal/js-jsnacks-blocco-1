7d71a79fcc651caee93d7d8dbfb1758eb660f992
Vanessa D'Alessandro <leavesthatfall@gmail.com>
2020-11-10 14:01:32 +0100

    wip

---
+++ snack4/js/script.js
@@ -0,0 +1,32 @@
+// chiediamo all’utente con 2 prompt due numeri tra 0 e la lunghezza dell’array
+// quindi creiamone uno nuovo con solo i valori che hanno la posizione compresa tra i due numeri inseriti dall’utente
+
+var myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
+
+console.log(myArray);
+
+
+var min=parseInt(prompt("inserisci un numero tra 0 e " + myArray.length));
+console.log(min);
+var max=parseInt(prompt("inserisci un numero tra 0 e " + myArray.length));
+console.log(max);
+
+var secondArray=[];
+
+function createNewArray(){
+    var i=0;
+
+    while (i<=myArray.length){
+       
+        var name=myArray[i]
+        var namePosition=myArray.indexOf[name]
+        console.log(namePosition)
+        if(namePosition>min && namePosition<max)
+        secondArray.push(name);
+        i++;
+    }
+    console.log(secondArray);
+    return secondArray
+}
+
+createNewArray();
